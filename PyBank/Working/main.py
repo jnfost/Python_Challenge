69b76db875ac4da1a601be0c73084f445e45c8f6
Jewell Foster <60074350+thinice209@users.noreply.github.com>
2020-03-23 10:45:08 -0400

    Update main.py
    found max profit and loss. assigned dates in for loop, but those variables aren't visible outside of the loop

--- PyBank/Working/main.py
+++ PyBank/Working/main.py
@@ -14,33 +14,64 @@ with open(budgetpath, 'r') as budgetfile:
     #for row in budgetreader:
     #    print(row)
 
+
 #find total number of months in data, set count to 0 and add 1 for each row in the file
     month_count = 0
     total_p_and_l = 0
-    #Create new list of just P&L values
+#Create new list of just P&L values
     p_and_l_list = []
     for row in budgetreader:
-    #Find total number of months in data    
+#Find total number of months in data    
         month_count += 1
     
 #Find net total amount of profit/losses
         total_p_and_l = total_p_and_l + int(row[1])
+#Make new list of just P&L values    
         p_and_l_list.append(int(row[1]))
-    
-    #print(month_count)
-    #print(total_p_and_l)
-    #print(p_and_l_list)
 
-    
+#print(month_count)
+#print(total_p_and_l)
+#print(p_and_l_list)
+
+#Find max P&L
+    max_profit = max(p_and_l_list)
+    print(max_profit)
+
+#Find min P&L
+    max_loss = min(p_and_l_list)
+    print(max_loss)   
+
+#Find dates for max profit and loss
+    for line in budgetreader:
+        if line[1] == max_profit:
+            max_profit_date = line[0]
+            print(max_profit_date)
+        elif line[1] == max_loss:
+            max_loss_date = line[0]
+            print(max_loss_date)
+        else:
+            break
+            
 #Find average of changes over entire period
-#Individual change: P&L from r + 1 - P&L from r (don't include last row) - p_and_l_change
+#Individual change: P&L from i + 1 - P&L from i (don't include last row) - p_and_l_change
 
     p_and_l_change = []
     for i in range(len(p_and_l_list)-1):
-        p_and_l_change.append([p_and_l_list[i+1] - p_and_l_list[i]])
-
-    #print(p_and_l_change)
+    #p_and_l_change.append([p_and_l_list[i+1] - p_and_l_list[i]])
+        p_and_l_change.append([int(p_and_l_list[i+1]) - int(p_and_l_list[i])])
+#print(p_and_l_change)
 
 #Total change (sum of all changes)
+    #total_change = sum(p_and_l_change)
 #Average change/total months-1
-    total_change = 0
+    #total_change = 0
+
+    #for item in p_and_l_change:
+        #total_change += item
+    #print(total_change)
+    print("Financial Analysis: ")
+    print("---------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Average Change: ")
+    #print(f"Greatest Increase in Profits: {max_profit_date}  {max_profit}")
+    #print(f"Greatest Decrease in Losses: {max_loss_date}  {max_loss}")
