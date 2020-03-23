79c628a8a88198df108311cf5389f95f38cb5629
Jewell Foster <60074350+thinice209@users.noreply.github.com>
2020-03-23 18:40:55 -0400

    Almost done
    Just need to find months for max profit and loss and change to currency format

--- PyBank/Working/2main.py
+++ PyBank/Working/2main.py
@@ -15,8 +15,9 @@ with open(budgetpath, 'r') as budgetfile:
     total_p_and_l = 0
 #Create new empty list of just P&L values
     p_and_l_list = []
-#Create new empty list for change in P&L
-    p_and_l_change = 0
+
+#Creat new empty list for dates
+    dates = []
     for row in budgetreader:
 #Find total number of months in data    
         row[1] = int(row[1])
@@ -24,25 +25,52 @@ with open(budgetpath, 'r') as budgetfile:
     
 #Find net total amount of profit/losses
         total_p_and_l = total_p_and_l + int(row[1])
+#Make new list of dates
+        dates.append(row[0])
 #Make new list of just P&L values    
         p_and_l_list.append(int(row[1]))
-        #p_and_l_change = int([row+ 1][1]) - int([row[1]])
 
-        max_profit = max(p_and_l_list)
-        max_loss = min(p_and_l_list)
 
-        if row[1] == max_profit:
-            max_profit_date = row[0]
-            print(row[0])
-        elif row[1] == max_loss:
-            max_loss_date = row[0]
-            print(row[0])
-        
-print(month_count)
-print(total_p_and_l)
+#Create new empty list for change in P&L
+    p_and_l_change = [0]
+#Make new list for P&L changes    
+    for i in range(len(p_and_l_list)-1):
+    #p_and_l_change.append([p_and_l_list[i+1] - p_and_l_list[i]])
+        p_and_l_change.append(int(p_and_l_list[i+1]) - int(p_and_l_list[i]))
+
+#Zip all columns together to make new list
+    new_budget_data = zip(dates, p_and_l_list, p_and_l_change)
+    new_budget_list = list(new_budget_data)
+    #print(new_budget_list)
+#Find max profit and max loss
+    max_profit = max(p_and_l_change)
+    #print(max_profit)
+    max_loss = min(p_and_l_change)
+    #print(max_loss)
+
+#Total change (sum of all changes)
+    total_change = 0
+
+    for item in p_and_l_change:
+        total_change = total_change + int(item)
+
+    #print(total_change)
+#Average change/total months-1
+    avg_change = total_change / (month_count - 1)
+    #print(avg_change)
+
+#print(month_count)
+#print(total_p_and_l)
+#print(dates)
 #print(p_and_l_list)
-print(p_and_l_change)
-print(max_profit)
-print(max_loss)
-print(max_profit_date)
-print(max_loss_date)
+#print(p_and_l_change)
+#print(new_budget_data)
+
+
+
+    print("Financial Analysis: ")
+    print("---------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Average Change: {avg_change}")
+    #print(f"Greatest Increase in Profits: {max_profit_date}  {max_profit}")
+    #print(f"Greatest Decrease in Losses: {max_loss_date}  {max_loss}")
